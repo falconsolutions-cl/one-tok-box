ff62747c9148588608b5dfe24b4f1ba964c87d7a
Marlon Falcon Hernandez <falconsoft.3d@gmail.com>
2017-01-24 22:59:20 -0300

    menu

--- menu.php
+++ menu.php
@@ -14,17 +14,31 @@
 		</div>
 
 		<!-- Collect the nav links, forms, and other content for toggling -->
-		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">			
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		<!--			
 			<form class="navbar-form navbar-left" method="GET" role="search">
 				<div class="form-group">
 					<input type="text" name="q" class="form-control" placeholder="Search">
 				</div>
 				<button type="submit" class="btn btn-default"><i class="glyphicon glyphicon-search"></i></button>
 			</form>
+	    -->		
 			<ul class="nav navbar-nav navbar-right">
-			    <li><a href="login.php" class="text-blanco">One box</a></li>
-				<li><a href="login.php">Salir</a></li>
+			    <li><a href="login.php">One box</a></li>
+				<li><a href="login.php"><span class="glyphicon glyphicon-download-alt" aria-hidden="true"></span> Salir</a></li>
 				</ul>
 			</div><!-- /.navbar-collapse -->
 		</div><!-- /.container-fluid -->
 	</nav>|
+
+	<style>
+		.navbar-nav > li > a {
+    color: #fff !important;
+}
+
+
+.navbar-brand {
+    color: #fff !important;
+}
+
+	</style>
