d40e50dd2d37cf46f1b5170cb0002552e98e7619
Marlon Falcon Hernandez <falconsoft.3d@gmail.com>
2017-02-03 23:48:11 -0300

    1
    1

---
+++ server/online.php
@@ -0,0 +1,40 @@
+<?php include("header.php") ?>
+<?php include("menu.php") ?>
+<body>  
+	<div class="container">
+
+		<div class="row">
+			<div class="col-lg-10">
+				<a href="index.php">Inicio</a> / <a href="Dashboard.php" >One-line</a>
+				<h4><strong>One-line</strong></h4>
+			</div>
+			<div class="col-lg-2 left">
+
+<div class="dropdown">
+  <a class="btn btn-warning" href="#" role="button"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Cerrar</a>
+</div>
+
+
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-lg-12 my-padding">			
+
+			<div class="row"> 
+			   <div class="col-lg-6">
+				   <img src="assets/img/p1.jpg" alt="" class="img-responsive">
+			   </div>
+
+
+			   <div class="col-lg-6">
+				   <img src="assets/img/p2.jpg" alt="" class="img-responsive">
+			   </div>				
+			</div>	
+			</div>
+		</div>
+
+	</div>      
+
+</body>
+<?php include("footer.php") ?>
